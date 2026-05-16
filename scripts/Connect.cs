4752d95dce4609e6701af8e5e456e130eb325ddf
HttpRafa <60099368+HttpRafa@users.noreply.github.com>
2026-05-16 13:59:21 +0200

    feat: Implement basic dashboard

---
+++ scripts/Connect.cs
@@ -0,0 +1,14 @@
+using Godot;
+
+namespace CarClient.Scripts;
+
+public partial class Connect : Control
+{
+	public override void _Ready() {
+		
+	}
+	
+	public override void _Process(double delta) {
+		
+	}
+}
--- scripts/Main.cs
+++ scripts/Main.cs
@@ -4,6 +4,11 @@ namespace CarClient.Scripts;
 
 public partial class Main : Control
 {
+	[Export] private TextureRect _forwards;
+	[Export] private TextureRect _backwards;
+	[Export] private TextureRect _left;
+	[Export] private TextureRect _right;
+	
 	public override void _Ready() {
 		GD.Print("Starting Car Client...");
 	}
