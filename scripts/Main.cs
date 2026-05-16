c19717d90c89d416d65de924c401926277a315c1
HttpRafa <60099368+HttpRafa@users.noreply.github.com>
2026-05-16 12:29:14 +0200

    feat: Add C# project

---
+++ scripts/Main.cs
@@ -0,0 +1,14 @@
+using Godot;
+
+namespace CarClient.Scripts;
+
+public partial class Main : Control
+{
+	public override void _Ready() {
+		GD.Print("Starting Car Client...");
+	}
+	
+	public override void _Process(double delta) {
+		
+	}
+}
